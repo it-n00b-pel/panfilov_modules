fcce0ee93e652a2bc2aa45c11184835b9ca30554
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-08-22 11:12:19 +0300

    add checkFinish/refactor

--- module_2/src/main.ts
+++ module_2/src/main.ts
@@ -1,7 +1,8 @@
 import {getBgColor} from './utils/getBgColor.ts';
 
-const gameField: Array<Array<0 | number>> = [];
+let gameField: Array<Array<0 | number>> = [];
 let score = 0;
+let isFinish = false;
 
 const createGameField = () => {
     for (let i = 0; i < 5; i++) {
@@ -36,7 +37,6 @@ function getRandomNumber() {
 
 // После каждого изменения массива gameField вызываем для перерисовки новых данных
 const renderData = () => {
-    console.log(gameField);
     for (let x = 0; x < 5; x++) {
         for (let y = 0; y < 5; y++) {
             let div = document.getElementById('c' + x + y);
@@ -57,8 +57,18 @@ const renderData = () => {
 
     let pElementResult = document.querySelector('.result p');
     if (pElementResult) pElementResult.innerHTML = score.toString();
+
+    if (isFinish) {
+        let div = document.getElementById('gameOver');
+        let spanRes = document.getElementById('finishResult');
+        if (div && spanRes) {
+            spanRes.innerHTML = score.toString();
+            div.style.display = 'flex';
+        }
+    }
 };
 
+// Находим идндекс след ячейки в зависимости от напрвлени джвижения
 const getNextIndexCell = (direction: 'left' | 'up' | 'down' | 'right', x: number, y: number) => {
     switch (direction) {
         case 'left':
@@ -85,12 +95,7 @@ const getNextIndexCell = (direction: 'left' | 'up' | 'down' | 'right', x: number
             return -1;
     }
 };
-
-const keyPressOnceTracker = () => {
-    window.addEventListener('keydown', handleKeydown, {once: true});
-};
 const moveLeft = () => {
-    console.log('ArrowLeft');
     const oldGameField = String(gameField);
 
     // Сначала проходимся по строкам / внутри них по столбцам (горизонталь)
@@ -116,15 +121,16 @@ const moveLeft = () => {
 
     const newGameField = String(gameField);
 
-    // Если не меняется местоположение или нет нового значения
+    // Если меняется местоположение или нового значения
+    // Если меняется местоположение или нового значения
     if (oldGameField !== newGameField) {
         getRandomNumber();
+        checkGameOver();
         renderData();
     }
 };
 
 const moveRight = () => {
-    console.log('ArrowRight');
     const oldGameField = String(gameField);
 
     // Сначала проходимся по строкам / внутри них по столбцам (горизонталь)
@@ -150,15 +156,15 @@ const moveRight = () => {
 
     const newGameField = String(gameField);
 
-    // Если не меняется местоположение или нет нового значения
+    // Если меняется местоположение или нового значения
     if (oldGameField !== newGameField) {
         getRandomNumber();
+        checkGameOver();
         renderData();
     }
 
 };
 const moveUp = () => {
-    console.log('ArrowUp');
     const oldGameField = String(gameField);
 
     // Сначала проходимся по строкам / внутри них по столбцам (вертикаль)
@@ -184,16 +190,15 @@ const moveUp = () => {
 
     const newGameField = String(gameField);
 
-    // Если не меняется местоположение или нет нового значения
+    // Если меняется местоположение или нового значения
     if (oldGameField !== newGameField) {
         getRandomNumber();
+        checkGameOver();
         renderData();
     }
 };
 
 const moveDown = () => {
-    console.log('ArrowDown');
-
     const oldGameField = String(gameField);
 
     // Сначала проходимся по строкам / внутри них по столбцам (вертикаль)
@@ -218,11 +223,42 @@ const moveDown = () => {
     }
     const newGameField = String(gameField);
 
-    // Если не меняется местоположение или нет нового значения
+    // Если меняется местоположение или нового значения
     if (oldGameField !== newGameField) {
         getRandomNumber();
+        checkGameOver();
         renderData();
     }
+};
+
+//Проверка на окончание игры
+// если по горизонтали/вертикали есть совпадеие или нули = еще играем
+const checkGameOver = () => {
+    for (let x = 0; x < 5; x++) {
+        for (let y = 0; y < 5; y++) {
+            if (gameField[x][y] === 0) {
+                return;
+            }
+            // Проверка есть одинаковые в ряду
+            if (y < 4) {
+                if (gameField[x][y] === gameField[x][y + 1]) {
+                    return;
+                }
+            }
+            // Проверка есть одинаковые в столбце
+            if (x < 4) {
+                if (gameField[x][y] === gameField[x + 1][y]) {
+                    return;
+                }
+            }
+        }
+    }
+    isFinish = true;
+    return;
+};
+
+const keyPressOnceTracker = () => {
+    window.addEventListener('keydown', handleKeydown, {once: true});
 };
 
 const handleKeydown = (event: KeyboardEvent) => {
@@ -246,11 +282,30 @@ const handleKeydown = (event: KeyboardEvent) => {
     keyPressOnceTracker();
 };
 
+const button = document.querySelector('.btnAgain');
+button && button.addEventListener('click', function () {
+    let div = document.getElementById('gameOver');
+    if (div) {
+        start();
+        div.style.display = 'none';
+    }
+});
+
 // ==== START
-createGameField();
-getRandomNumber();
-getRandomNumber();
-renderData();
-keyPressOnceTracker();
+
+function start() {
+    gameField = [];
+    score = 0;
+    isFinish = false;
+    createGameField();
+    getRandomNumber();
+    getRandomNumber();
+    renderData();
+    keyPressOnceTracker();
+}
+
+start();
+
+
 
 
