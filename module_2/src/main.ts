f3dc822d74975fa3a93758d1f1603efed721e510
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-08-24 12:13:10 +0300

    refactor

--- module_2/src/main.ts
+++ module_2/src/main.ts
@@ -10,6 +10,7 @@ import {
     readFromLocalStorage,
     saveToLocalStorage,
 } from './utils/localStorage.ts';
+import {keyPressOnceTracker} from './utils/listeners.ts';
 
 let gameField: Array<Array<0 | number>> = [];
 let score = 0;
@@ -153,7 +154,7 @@ const getNextIndexCell = (direction: 'left' | 'up' | 'down' | 'right', x: number
             return -1;
     }
 };
-const moveLeft = () => {
+export const moveLeft = () => {
     const oldGameField = String(gameField);
 
     // Сначала проходимся по строкам / внутри них по столбцам (горизонталь)
@@ -189,7 +190,7 @@ const moveLeft = () => {
     }
 };
 
-const moveRight = () => {
+export const moveRight = () => {
     const oldGameField = String(gameField);
 
     // Сначала проходимся по строкам / внутри них по столбцам (горизонталь)
@@ -208,7 +209,6 @@ const moveRight = () => {
                     score += gameField[x][y];
                     currentValue = score;
                     check2048(gameField[x][y]);
-
                 }
             } else {
                 break;
@@ -226,7 +226,8 @@ const moveRight = () => {
     }
 
 };
-const moveUp = () => {
+
+export const moveUp = () => {
     const oldGameField = String(gameField);
 
     // Сначала проходимся по строкам / внутри них по столбцам (вертикаль)
@@ -262,7 +263,7 @@ const moveUp = () => {
     }
 };
 
-const moveDown = () => {
+export const moveDown = () => {
     const oldGameField = String(gameField);
 
     // Сначала проходимся по строкам / внутри них по столбцам (вертикаль)
@@ -299,7 +300,10 @@ const moveDown = () => {
 
 //Проверка есть ли результат 2048
 const check2048 = (num: number) => {
-    num === winNum ? isWin = true : isWin = false;
+    // num === winNum ? isWin = true : isWin = false;
+    if (num === winNum) {
+        isWin = true;
+    }
 };
 
 //Проверка на окончание игры
@@ -328,126 +332,6 @@ const checkGameOver = () => {
     isFinish = true;
     return;
 };
-
-const keyPressOnceTracker = () => {
-    window.addEventListener('keydown', handleKeydown, {
-        once: true,
-    });
-};
-
-const handleKeydown = (event: KeyboardEvent) => {
-    switch (event.key) {
-        case 'ArrowUp':
-            moveUp();
-            break;
-        case 'ArrowRight':
-            moveRight();
-            break;
-        case 'ArrowDown':
-            moveDown();
-            break;
-        case 'ArrowLeft':
-            moveLeft();
-            break;
-        default:
-            keyPressOnceTracker();
-            return;
-    }
-    keyPressOnceTracker();
-};
-
-let resetButtons = document.getElementsByClassName('btnAgain');
-
-// Функция-обработчик события click
-function handleClick() {
-    resetGame();
-}
-
-// Назначаем обработчик события для каждой кнопки
-for (let i = 0; i < resetButtons.length; i++) {
-    resetButtons[i].addEventListener('click', handleClick);
-}
-
-
-// Для управления мышкой/жестами находим где было нажатие и где отпустили/ вычисляем направление
-let startX: number, startY: number, endX: number, endY: number;
-
-document.addEventListener('mousedown', (event) => {
-    startX = event.x;
-    startY = event.y;
-});
-
-document.addEventListener('mouseup', (event) => {
-    endX = event.x;
-    endY = event.y;
-    console.log(endX + '-' + endY);
-
-    let x = endX - startX;
-    let y = endY - startY;
-
-    let absX = Math.abs(x) > Math.abs(y);
-    let absY = Math.abs(y) > Math.abs(x);
-    if (x > 0 && absX) {
-        moveRight();
-    } else if (x < 0 && absX) {
-        moveLeft();
-    } else if (y > 0 && absY) {
-        moveDown();
-    } else if (y < 0 && absY) {
-        moveUp();
-    }
-});
-
-
-document.addEventListener('touchstart', (event) => {
-    startX = event.touches[0].pageX;
-    startY = event.touches[0].pageY;
-});
-
-document.addEventListener('touchend', (event) => {
-    endX = event.changedTouches[0].pageX;
-    endY = event.changedTouches[0].pageY;
-
-    let x = endX - startX;
-    let y = endY - startY;
-
-    let absX = Math.abs(x) > Math.abs(y);
-    let absY = Math.abs(y) > Math.abs(x);
-    if (x > 0 && absX) {
-        moveRight();
-    } else if (x < 0 && absX) {
-        moveLeft();
-    } else if (y > 0 && absY) {
-        moveDown();
-    } else if (y < 0 && absY) {
-        moveUp();
-    }
-});
-
-// Открытие списка результатов
-let listData = document.getElementsByClassName('tableData__img');
-
-
-// Назначаем обработчик события для кнопки открытия списка
-listData[0].addEventListener('click', (event) => {
-    let div = document.getElementsByClassName('recordList');
-    // @ts-ignore
-    div[0].style.left = '0px';
-    event.stopPropagation();
-});
-
-
-const div = document.getElementsByClassName('recordList');
-
-document.addEventListener('click', (e) => {
-    const withinBoundaries = e.composedPath().includes(div[0]);
-
-    if (!withinBoundaries) {
-        // @ts-ignore
-        div[0].style.left = '-330px'; // скрываем элемент т к клик был за его пределами
-    }
-});
-
 
 export const resetGame = () => {
 
@@ -486,6 +370,7 @@ function start() {
     }
 
     isFirstStart = true;
+    isWin = false;
 
     startTimer();
     gameField = [];
